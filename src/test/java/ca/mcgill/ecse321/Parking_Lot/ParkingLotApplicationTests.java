43d899ec14c0f26ff881155ac1a1543a3c7f14a7
Ahmed Bachir <ahmed.bachir@mail.mcgill.ca>
2023-03-05 11:37:40 -0500

    first commit

---
+++ src/test/java/ca/mcgill/ecse321/Parking_Lot/ParkingLotApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.Parking_Lot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingLotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
